11818735ecc8ee69579ddac02ca1fdcfea563988
Rodrigo Bravo C <rodrigo.sebastian.bravo.cea@kyndryl.com>
2022-07-06 11:25:35 -0400

    test previred

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -2,6 +2,6 @@ import {get} from '@loopback/rest';
 export class HelloController {
   @get('/hello')
   hello(): string {
-    return 'Hola Mundo 2------ !!';
+    return 'Test Previred !!!!!!! !!';
   }
 }
